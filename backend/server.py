947c467070437d4c02c7b38c7e831b3442b08a6a
JiaGengChang <jiageng1997@gmail.com>
2025-05-25 22:50:06 +0800

    minor fix

--- backend/server.py
+++ backend/server.py
@@ -57,7 +57,7 @@ if __name__ == "__main__":
     import uvicorn
     uvicorn.run(
         app, 
-        host="0.0.0.0", 
+        host="127.0.0.1", 
         port=8000,
         timeout_keep_alive=60 
     )
