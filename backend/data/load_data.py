435d61d9f94ad504bea2f82770fa2c087ffbe72d
JiaGengChang <jiageng1997@gmail.com>
2025-05-26 10:06:40 +0800

    calculate derived metrics

--- backend/data/load_data.py
+++ backend/data/load_data.py
@@ -1,17 +1,22 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 import time
 import pandas as pd
 import mysql.connector
-from metadata import column_mapping
+from metadata import column_mapping, derived_columns
 
-# script to establish company financial info MySQL database
+# script to create and populate MySQL database of company financial data
+# that will be queried by the LangChain agent
+# db description resides in backend/data/agent.py
 
-start_time = time.time()  # ⏱ Start timer
+start_time = time.time()
 
 db_config = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': os.getenv("MYSQL_ROOT_PASSWORD"),
+    'host': os.getenv('MYSQL_HOST', 'localhost'),
+    'user': os.getenv('MYSQL_USER', 'root'),
+    'port': int(os.getenv('MYSQL_PORT', 3306)),
+    'password': os.getenv('MYSQL_PASSWORD'),
     'database': 'financial_db'
 }
 
@@ -31,39 +36,43 @@ cursor = conn.cursor()
 
 columns = df.columns.tolist()
 
-# Create table if it doesn't exist
-create_table_query = f"""
+# Create table with company metadata and base financial data
+create_table_query = f'''
 CREATE TABLE IF NOT EXISTS company_data (
-    {', '.join([f"{col} VARCHAR(255)" for col in columns])},
+    {', '.join([f"{col} VARCHAR(255)" for col in columns + derived_columns])},
     PRIMARY KEY (company_id, year)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-"""
+);
+'''
 cursor.execute(create_table_query)
 
-# Insert each row
+# Insert company and base financial data
 for _, row in df.iterrows():
     values = [row[col] if pd.notna(row[col]) else None for col in columns]
 
-    insert_query = f"""
+    insert_query = f'''
         INSERT INTO company_data ({', '.join(columns)})
         VALUES ({', '.join(['%s'] * len(columns))})
         ON DUPLICATE KEY UPDATE
         {', '.join(f"{col} = VALUES({col})" for col in columns if col not in ['company_id', 'year'])}
-    """
+    '''
     cursor.execute(insert_query, values)
 
 conn.commit()
-print(f"{len(df)} rows inserted successfully")
+print(f'{len(df)} rows inserted successfully')
 
-# Run stored procedure
+# Create derived financial columns
+# Procedure to create derived financial data
+with open(os.path.join(os.path.dirname(__file__), 'calculate_financial_data.sql'), 'r') as f:
+    create_procedure_query = f.read()
+cursor.execute('DROP PROCEDURE IF EXISTS calculate_financial_data')
+cursor.execute(create_procedure_query)
 cursor.callproc('calculate_financial_data')
 conn.commit()
-print(f"Financial ratios updated successfully")
+print(f'Derived financial metrics updated successfully')
 
 # Cleanup
 cursor.close()
 conn.close()
 
-# ⏱ End timer
 end_time = time.time()
-print(f"⏱ Time taken: {end_time - start_time:.2f} seconds")
+print(f'⏱ Time taken: {end_time - start_time:.2f} seconds')
