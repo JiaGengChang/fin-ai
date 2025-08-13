4b25d0930aed83982ddd2d9cb3d56d0334550265
JiaGengChang <jiageng1997@gmail.com>
2025-08-13 11:24:42 +0800

    async init chat

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import uuid
 from fastapi import FastAPI
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, PlainTextResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from agent import init_agent, query_agent 
@@ -25,14 +25,17 @@ static_dir = os.path.join(app_dir, 'static')
 app.mount("/graph", StaticFiles(directory=graph_folder), name="graph")
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
+# "gate" to ensure model first receives init prompt
+app.state.init_prompt_done = asyncio.Event()
+
 class Query(BaseModel):
     user_input: str
 
 @app.post("/api/ask")
 async def ask(query: Query):
-
+    global graph_folder
     _uuid = str(uuid.uuid4().hex[:8])
-    graph_full_path = os.path.join(graph_folder, f"/graph_{_uuid}.png")
+    graph_full_path = os.path.join(graph_folder, f"graph_{_uuid}.png")
     full_prompt = f"""
     DEVELOPER PROMPT:
     If a graph is generated, save the graph as {graph_full_path}. Display the image with `<img src={graph_full_path} max-width=100% height=auto>`.  If multiple graphs are generated, add suffixes to the filenames and update the img src attribute accordingly.
@@ -40,26 +43,23 @@ async def ask(query: Query):
     USER MESSAGE:
     {query.user_input}
     """
-
     response = query_agent(full_prompt)
-
-    graph_url = None
-    
-    if os.path.exists(graph_full_path):
-        graph_url = f"graph/{os.path.basename(graph_full_path)}"
-
-    print(f"\n\nGRAPH URL : {graph_url}\n\n")
-    return {
-        "response": response,
-        "graph_url": graph_url
-    }
+    return {"response": response}
 
 
 # Serve landing page
 @app.get("/")
 async def serve_frontend():
-    await init_agent()
-    return FileResponse("static/index.html")
+    response = FileResponse("static/index.html")
+    # Create task to send init prompt
+    asyncio.create_task(init_agent(app))
+    return response
+
+# retrieve response to init prompt
+@app.post("/api/init")
+async def get_init_response():
+    await app.state.init_prompt_done.wait()
+    return PlainTextResponse(app.state.init_response)
 
 if __name__ == "__main__":
     import uvicorn
